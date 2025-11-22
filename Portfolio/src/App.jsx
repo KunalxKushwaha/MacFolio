bcf1e7a1a42cc098f234cb9bbf22f68a4246d464
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-23 02:07:00 +0530

    tailwind added

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my MacOSFolio</h1>
+      <h1 className='text-3xl font-medium '>Welcome to my MacOSFolio</h1>
     </div>
   )
 }
