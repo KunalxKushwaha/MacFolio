eb5c3e6fe03d9a6496c262308002e1e65c6b383d
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-23 13:24:21 +0530

    Making the Navbar

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 const Navbar = () => {
+
+    const data = [
+            {id: 1, name: "Portfolio"},
+            {id: 2, name: "Contact"},
+            {id: 3, name: "Projects"},
+        ];
   return (
-    <div>
-      Navbar
+   <nav>
+     <div>
+      <img src="/images/logo.svg" alt="logo" />
+      <p className='font-bold'>Kunal's Portfolio</p>
+
+      <ul>
+        {data.map(({id,name}) => (
+            <li key={id}>
+                <p>{name}</p>
+            </li>
+        ))}
+      </ul>
     </div>
+   </nav>
   )
 }
 
