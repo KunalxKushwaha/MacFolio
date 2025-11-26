0a4fc691804e67c81c72a1f48450920e0ed5864a
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-26 12:53:22 +0530

    fixed the bug

--- Portfolio/src/windows/Resume.jsx
+++ Portfolio/src/windows/Resume.jsx
@@ -1,64 +1,30 @@
-// import WindowControls from '#components/WindowControls';
-// import WindowWrapper from '#hoc/WindowWrapper'
-// import { Download } from 'lucide-react';
-// import React from 'react'
-// import { Document, Page, pdfjs } from 'react-pdf';
-// import "react-pdf/dist/Page/AnnotationLayer.css";
-// import "react-pdf/dist/Page/TextLayer.css";
-
-// pdfjs.GlobalWorkerOptions.workerSrc =
-//   `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-// const Resume = () => {
-//   return (
-//     <>
-//     <div id='window-header'>
-//       <WindowControls target="resume"/>
-//       <h2>Resume.pdf</h2>
-//       <a href="files/Resume2.pdf" download className='cursor-pointer' title='Download Resume'>
-//       <Download className='icon'/></a>
-//     </div>
-//     <Document file="/files/Resume2.pdf">
-//       <Page pageNumber={1} renderTextLayer renderAnnotationLayer/>
-     
-//     </Document>
-//     </>
-//   )
-// }
-
-// const ResumeWindow = WindowWrapper(Resume, "resume");
-
-// export default ResumeWindow
 import WindowControls from '#components/WindowControls';
-import WindowWrapper from '#hoc/WindowWrapper';
+import WindowWrapper from '#hoc/WindowWrapper'
 import { Download } from 'lucide-react';
-import React from 'react';
+import React from 'react'
 import { Document, Page, pdfjs } from 'react-pdf';
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-// FIX for Vercel
-pdfjs.GlobalWorkerOptions.workerSrc =
-  `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const Resume = () => {
   return (
     <>
-      <div id="window-header">
-        <WindowControls target="resume" />
-        <h2>Resume.pdf</h2>
-
-        <a href="/files/Resume2.pdf" download title="Download Resume">
-          <Download className="icon" />
-        </a>
-      </div>
-
-      <Document file="/files/Resume2.pdf">
-        <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} />
-      </Document>
+    <div id='window-header'>
+      <WindowControls target="resume"/>
+      <h2>Resume.pdf</h2>
+      <a href="files/Resume2.pdf" download className='cursor-pointer' title='Download Resume'>
+      <Download className='icon'/></a>
+    </div>
+    <Document file="files/Resume2.pdf">
+      <Page pageNumber={1} renderTextLayer renderAnnotationLayer/>
+     
+    </Document>
     </>
-  );
-};
+  )
+}
 
 const ResumeWindow = WindowWrapper(Resume, "resume");
-export default ResumeWindow;
+
+export default ResumeWindow
