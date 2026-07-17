c66aa488bb1ca8522553730e34d2468b4f7d3e45
KunalxKushwaha <kushwahakunal733@gmail.com>
2026-07-17 20:40:47 +0530

    changed resume

--- Portfolio/src/windows/Resume.jsx
+++ Portfolio/src/windows/Resume.jsx
@@ -14,10 +14,10 @@ const Resume = () => {
     <div id='window-header'>
       <WindowControls target="resume"/>
       <h2>Resume.pdf</h2>
-      <a href="files/KunalxKushwaha3.pdf" download className='cursor-pointer' title='Download Resume'>
+      <a href="files/Kunal.pdf" download className='cursor-pointer' title='Download Resume'>
       <Download className='icon'/></a>
     </div>
-    <Document file="files/KunalxKushwaha3.pdf">
+    <Document file="files/Kunal.pdf">
       <Page pageNumber={1} renderTextLayer renderAnnotationLayer/>
      
     </Document>
